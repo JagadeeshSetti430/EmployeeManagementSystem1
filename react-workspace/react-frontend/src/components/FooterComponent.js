081dd1698efafeeb5a03b68f9905d59769ede664
Jagadeesh Setti <jagadeeshsetti430@gmail.com>
2025-04-05 23:21:54 +0530

    frontend

---
+++ react-workspace/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css';
+
+function FooterComponent() {
+  return (
+        <footer className='footer'>
+            <small>All Right Reserved &copy; by Jagadeesh Setti</small>
+        </footer>
+  )
+}
+
+export default FooterComponent
